07b51184fe3cb0385f0bb458d25cb69e85bb5d18
Loc <hpkhanhloc@gmail.com>
2019-06-18 14:23:06 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,23 +4,26 @@ import requests
 import os
 
 app = Flask(__name__)
-port = port = int(os.environ["PATH"])
+port = int(os.environ["PORT"])
+print(port)
 
 @app.route('/', methods=['POST'])
 def index():
-  data = json.loads(request.get_data())
+  print(port)
+  data = json.loads(request.get_data().decode('utf-8'))
 
   # FETCH THE CRYPTO NAME
-  crypto_name = data['conversation']['memory']['crypto']['raw']
+  crypto_name = data["nlp"]["entities"]["crypto_name"][0]["raw"]
+  crypto_ticker = crypto_name.upper()
 
   # FETCH BTC/USD/EUR PRICES
-  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
+  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_ticker+"&tsyms=BTC,USD,EUR")
 
   return jsonify(
     status=200,
     replies=[{
       'type': 'text',
-      'content': 'The price of %s is %f BTC and %f USD' % (crypto_name, r.json()['BTC'], r.json()['USD'])
+      'content': 'The price of %s is :\n%f BTC, \n%f USD, and \n%f EUR.' % (crypto_ticker, r.json()['BTC'], r.json()['USD'], r.json()['EUR'])
     }]
   )
 
