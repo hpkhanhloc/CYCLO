5b73a0dcf6784c111a69693d1f73748ae24ad9cc
Loc <hpkhanhloc@gmail.com>
2019-06-20 16:06:31 +0300

    Testing

--- application.py
+++ application.py
@@ -1,35 +1,28 @@
 from flask import Flask, request, jsonify
+import pyodbc
 import json
-import requests
-import os
-
 app = Flask(__name__)
-port = int(os.environ["PORT"])
-print(port)
 
-@app.route('/', methods=['POST'])
-def index():
-  print(port)
-  data = json.loads(request.get_data().decode('utf-8'))
+@app.route("/")
+def main():
+    server = 'cyclo.database.windows.net'
+    database = 'TutorialDB'
+    username = 'cycloadmin'
+    password = 'Cyclosummer2019'
+    driver= '{ODBC Driver 17 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    #Sample select query
+    cursor.execute("SELECT * FROM dbo.Customers;") 
+    row = cursor.fetchone()
+    thislist =[]
+    while row:
+        x = {"id":str(row[0]),"name":str(row[1]),"location":str(row[2]),"mail":str(row[3])}
+        thislist.append(x)
+        row = cursor.fetchone()
+    j = json.dumps(thislist)
+    return j
 
-  # FETCH THE CRYPTO NAME
-  crypto_name = data["nlp"]["entities"]["crypto_name"][0]["raw"]
-  crypto_ticker = crypto_name.upper()
+if __name__ == "__main__":
+    app.run()
 
-  # FETCH BTC/USD/EUR PRICES
-  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_ticker+"&tsyms=BTC,USD,EUR")
-
-  return jsonify(
-    status=200,
-    replies=[{
-      'type': 'text',
-      'content': 'The price of %s is :\n%f BTC, \n%f USD, and \n%f EUR.' % (crypto_ticker, r.json()['BTC'], r.json()['USD'], r.json()['EUR'])
-    }]
-  )
-
-@app.route('/errors', methods=['POST'])
-def errors():
-  print(json.loads(request.get_data()))
-  return jsonify(status=200)
-
-app.run(port=port, host="0.0.0.0")
