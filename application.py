a5e96c1d0625a78ca8744f6f5740a67765633935
Loc <hpkhanhloc@gmail.com>
2019-06-23 18:07:42 +0300

    Update application.py

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ def index():
     data = json.loads(request.get_data().decode('utf-8'))
     customerid = data["nlp"]["entities"]["number"][0]["raw"]
     #Sample select query
-    cursor.execute("SELECT * FROM dbo.Customers WHERE CustomerId="+customerid+";") 
+    cursor.execute("SELECT * FROM dbo.Customers WHERE CustomerId=2;") 
     row = cursor.fetchone()
     thislist =[]
     while row:
