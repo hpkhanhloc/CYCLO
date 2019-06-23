4d1da99714150f8ab24e977ad91fadece2e11ef2
Loc <hpkhanhloc@gmail.com>
2019-06-23 21:51:40 +0300

    return

--- server.py
+++ server.py
@@ -1,35 +1,46 @@
 from flask import Flask, request, jsonify
+import pyodbc
 import json
 import requests
 import os
 
 app = Flask(__name__)
-port = int(os.environ["PORT"])
+port = int(os.environ['PORT'])
 print(port)
 
-@app.route('/', methods=['POST'])
+@app.route('/',methods=['POST'])
 def index():
-  print(port)
-  data = json.loads(request.get_data().decode('utf-8'))
+    server = 'cyclo.database.windows.net'
+    database = 'TutorialDB'
+    username = 'cycloadmin'
+    password = 'Cyclosummer2019'
+    driver= '{ODBC Driver 17 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    #Fetch the ID
+    #data = json.loads(request.get_data().decode('utf-8'))
+    #customerid = data["nlp"]["entities"]["number"][0]["raw"]
+    #Sample select query
+    cursor.execute("SELECT * FROM dbo.Customers WHERE CustomerId=2;") 
+    row = cursor.fetchone()
+    thislist =[]
+    while row:
+        x = {"id":str(row[0]),"name":str(row[1]),"location":str(row[2]),"mail":str(row[3])}
+        thislist.append(x)
+        row = cursor.fetchone()
+    j = json.dumps(thislist)
+    return jsonnify(
+        status=200,
+        replies=[{
+            'type':'text',
+            'content': 'CustomerID: %s,\nName: %f,\nLocation: %f,\nEmail: %f.' % (customerid, j.json()['Name'], j.json()['Location'], j.json()['Email'])
+        }]    
+    )
 
-  # FETCH THE CRYPTO NAME
-  crypto_name = data["nlp"]["entities"]["crypto_name"][0]["raw"]
-  crypto_ticker = crypto_name.upper()
-
-  # FETCH BTC/USD/EUR PRICES
-  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_ticker+"&tsyms=BTC,USD,EUR")
-
-  return jsonify(
-    status=200,
-    replies=[{
-      'type': 'text',
-      'content': 'The price of %s is :\n%f BTC, \n%f USD, and \n%f EUR.' % (crypto_ticker, r.json()['BTC'], r.json()['USD'], r.json()['EUR'])
-    }]
-  )
-
-@app.route('/errors', methods=['POST'])
-def errors():
-  print(json.loads(request.get_data()))
+@app.route('/errors', methods=['POST']) 
+def errors(): 
+  print(json.loads(request.get_data())) 
   return jsonify(status=200)
 
 app.run(port=port, host="0.0.0.0")
+
