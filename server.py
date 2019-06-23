bf420d6382926ef1127c155c61dbedbd8c7a2dd7
Loc <hpkhanhloc@gmail.com>
2019-06-23 22:02:00 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,10 +5,10 @@ import requests
 import os
 
 app = Flask(__name__)
-port = int(os.environ['PORT'])
-print(port)
+#port = int(os.environ['PORT'])
+#print(port)
 
-@app.route('/',methods=['POST'])
+@app.route('/')
 def index():
     server = 'cyclo.database.windows.net'
     database = 'TutorialDB'
@@ -42,5 +42,5 @@ def errors():
   print(json.loads(request.get_data())) 
   return jsonify(status=200)
 
-app.run(port=port, host="0.0.0.0")
+app.run(port="1433", host="0.0.0.0")
 
