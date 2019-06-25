58fffc8c766e7c959d3685383c5dbffef40cf958
Loc <hpkhanhloc@gmail.com>
2019-06-25 15:15:36 +0300

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ import os
 
 app = Flask(__name__)
 port = int(os.environ['PORT'])
-print(port)
 
 @app.route('/',methods=['POST'])
 def index():
@@ -19,22 +18,20 @@ def index():
     cursor = cnxn.cursor()
     #Fetch the ID
     data = json.loads(request.get_data().decode('utf-8'))
-    customerid = int(2)
+    customerid = data['nlp']['entities']['number'][0]['raw']
     #Sample select query
     cursor.execute("SELECT * FROM dbo.Customers WHERE CustomerId=2;") 
     row = cursor.fetchone()
-    thislist =[]
     while row:
         x = {"id":str(row[0]),"name":str(row[1]),"location":str(row[2]),"mail":str(row[3])}
-        thislist.append(x)
         row = cursor.fetchone()
-    j = json.dumps(thislist)
+    j = json.dumps(x)
     r = json.loads(j)
     return jsonify(
         status=200,
         replies=[{
             'type':'text',
-            'content': 'CustomerID: %s,\nName: %s,\nLocation: %s,\nEmail: %s.' % (customerid, r['Name'], r['Location'], r['Email'])
+            'content': 'CustomerID: %s,\nName: %s,\nLocation: %s,\nEmail: %s.' % (customerid, r['name'], r['location'], r['mail']),
         }]    
     )
 
@@ -43,5 +40,5 @@ def errors():
   print(json.loads(request.get_data())) 
   return jsonify(status=200)
 
-app.run(port=port, host="0.0.0.0")
+app.run(port=port, host='0.0.0.0')
 
