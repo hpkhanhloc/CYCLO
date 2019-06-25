b616a40a1dba69d872b569f497421839f2cd773e
Loc <hpkhanhloc@gmail.com>
2019-06-26 00:28:57 +0300

    Update application.py

--- application.py
+++ application.py
@@ -7,8 +7,7 @@ import os
 app = Flask(__name__)
 port = '443'
 
-@app.route('/customer',methods=['POST'])
-def index():
+def dataconn():
     server = 'cyclo.database.windows.net'
     database = 'CYCLO'
     username = 'cycloadmin'
@@ -16,6 +15,11 @@ def index():
     driver= '{ODBC Driver 17 for SQL Server}'
     cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = cnxn.cursor()
+    return cursor
+
+@app.route('/customer',methods=['POST'])
+def index():
+    cursor = dataconn()
     #Fetch the ID
     data = json.loads(request.get_data().decode('utf-8'))
     customerid = data['nlp']['entities']['number'][0]['raw']
